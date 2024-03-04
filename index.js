0a70754b9ac4d2024026b131ae12f44b0fe5f217
Vaibhav Matere <vpm00718897@gmail.com>
2024-03-04 16:53:08 +0530

    create basic express server

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-// const express = require('express');
+const express = require('express');
+const cors = require('cors');
 
-// const app = express();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res, next) => {
+     res.json({ message: 'Hello!!' })
+});
+
+app.listen(8000, () => {
+     console.log('server is running on port 8000');
+})
